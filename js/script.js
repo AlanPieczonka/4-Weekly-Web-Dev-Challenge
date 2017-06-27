4c97d50cb41de663dff49543e4e1e207aa0d9ac7
Alan <alanpieczonka@gmail.com>
2017-06-27 14:55:39 +0200

    Mobile nav PS prototype and desktop hamburger functionality

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 $(document).ready(function() {
     console.log( "JS WORKS" );
     
-    var headerIsOpened = false; 
+    let nav__listIsOpened = true;
+    const header__hamburger = document.querySelector('.header__hamburger');
+    header__hamburger.addEventListener('click', function(){
+        if(nav__listIsOpened){
+          $('.nav__list').fadeOut(300);
+          nav__listIsOpened = false;
+        }else{
+          $('.nav__list').fadeIn(300);
+          nav__listIsOpened = true;
+        }
+    });
+    
+    //SEARCH INPUT HEADER
+    var header__inputIsOpened = false; 
 $('.li__icon--search').click(function(){
-    if(headerIsOpened === false){
+    if(header__inputIsOpened === false){
          $(".nav__line--first").addClass('closed');
          $( ".header__input" ).fadeIn( 300, function() {
-           headerIsOpened = true;
+           header__inputIsOpened = true;
         });
      }else{
            $( ".header__input" ).fadeOut( 300, function() {
-           headerIsOpened = false;
+           header__inputIsOpened = false;
         });
          setTimeout(function(){
            $(".nav__line--first").delay(1000).removeClass('closed');
